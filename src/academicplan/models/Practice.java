09a0cedf5b57e4842e4104c946179beb91794e99
Myravei <Myravei-290199@mail.ru>
2020-06-23 00:50:45 +0300

    Создал модель практики

---
+++ src/academicplan/models/Practice.java
@@ -0,0 +1,48 @@
+package academicplan.models;
+
+public class Practice {
+
+    private int id;
+    private int semester;
+    private int duration;
+    private String type;
+
+    public Practice(int id, int semester, int duration, String type) {
+        this.id = id;
+        this.semester = semester;
+        this.duration = duration;
+        this.type = type;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getSemester() {
+        return semester;
+    }
+
+    public void setSemester(int semester) {
+        this.semester = semester;
+    }
+
+    public int getDuration() {
+        return duration;
+    }
+
+    public void setDuration(int duration) {
+        this.duration = duration;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+}
