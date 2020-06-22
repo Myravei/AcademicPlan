8f48325250ba807090b3c4af55d72e7ef1e94e5c
Myravei <Myravei-290199@mail.ru>
2020-06-22 15:25:13 +0300

    Получуние дисциплин из БД

--- src/academicplan/database/DatabaseHandler.java
+++ src/academicplan/database/DatabaseHandler.java
@@ -12,4 +12,28 @@ public class DatabaseHandler extends Configs{
         Class.forName("com.mysql.cj.jdbc.Driver");
         return DriverManager.getConnection(connectionString, dbUser, dbPass);
     }
+
+    public ArrayList<Discipline> getDisciplines() {
+        ArrayList<Discipline> disciplines = new ArrayList<>();
+        try(
+                Statement statement = getDbConnection().createStatement();
+                ResultSet set = statement.executeQuery("Select * From " + Const.DISCIPLINE_TABLE)
+        ){
+            while (set.next()){
+                int id = set.getInt(Const.DISCIPLINE_ID);
+                int semester = set.getInt(Const.DISCIPLINE_SEMESTER);
+                String name = set.getString(Const.DISCIPLINE_NAME);
+                int lectures = set.getInt(Const.DISCIPLINE_LECTURES);
+                int practices = set.getInt(Const.DISCIPLINE_PRACTICES);
+                int laboratories = set.getInt(Const.DISCIPLINE_LABORATORIES);
+                int trudoemkost = set.getInt(Const.DISCIPLINE_TRUDOEMKOST);
+                String control = set.getString(Const.DISCIPLINE_CONTROL);
+                boolean coursework = set.getBoolean(Const.DISCIPLINE_COURSEWORK);
+                disciplines.add(new Discipline(id, semester, name, lectures, practices, laboratories, trudoemkost, control, coursework));
+            }
+        } catch (SQLException | ClassNotFoundException ex) {
+            ex.printStackTrace();
+        }
+        return disciplines;
+    }
 }
